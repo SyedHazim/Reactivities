a0f385196004d3227560528e7310cb5d710c150e
syedhazim0918 <syedhazim0918@gmail.com>
2025-02-14 05:51:56 +0530

    Initial Commit - API setup done

---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>( options=>{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultSqliteConnectionString"));
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await DBInitializer.SeedData(context);
+
+}
+catch(Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"Error occurred");
+}
+
+app.Run();
